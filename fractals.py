8dd0a57f9d16fefc383e72d3b696ec3bb6dc5fc4
sor0013 <126166129+sor0013@users.noreply.github.com>
2023-06-25 21:55:10 +0200

    Add files via upload

--- fractals.py
+++ fractals.py
@@ -1,214 +1,227 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.widgets as w
+from numba import jit
+
+@jit(nopython=True)
+def mandelbrot_set(mal_set, Z, C, k):
+    for i in range(k):
+        Z = Z**2 + C
+        mal_set += 1 * (np.abs(Z) < 2)
+    
+    return mal_set
+
+@jit(nopython=True)
+def julia_set(jul_set, Z, C, k):    
+    for i in range(k):
+        Z = Z**2 + C
+        jul_set += 1 * (np.abs(Z) < 2)
+    
+    return jul_set
 
 class Fractal:
     """Třída Fractal, jeden parametr 
 a to je n, které udává rozměr matice,
 která představuje množinu."""
-    def __init__(self, n):
-        self.x_min = -2
-        self.x_max = 2
-        self.y_min = -2
-        self.y_max = 2
-        self.n = n
-        self.k = 50
-        self.c_re = 0.0
-        self.c_im = 0.0
-        self.col = "hot"
-        self.fig, self.ax = plt.subplots()
-        self.im = self.ax.imshow(self.mandelbrot_set(), cmap="hot", extent=(self.x_min, self.x_max, self.y_min, self.y_max))
-        self.fun_name = self.mandelbrot_set
+    def __init__(self, n: int):
+        self.__S__ = {"x" : 0.0, "y" : 0.0}
+        self.__r__ = 2.0
+        self.__n__ = n
+        self.__k__ = 50
+        self.__c_re__ = 0.0
+        self.__c_im__ = 0.0
+        self.__col__ = "hot"
+        self.__fig__, self.__ax__ = plt.subplots()
+        self.__im__ = self.__ax__.imshow(self.__mandelbrot_set__(), cmap="hot", extent=(-2.0, 2.0, -2.0, 2.0))
+        self.__fun_name__ = self.__mandelbrot_set__
         
-    def mandelbrot_set(self):
-        k = self.k
-        x, y = np.linspace(self.x_min, self.x_max, self.n), np.linspace(self.y_min, self.y_max, self.n)
+    def __mandelbrot_set__(self):
+        x_min = self.__S__["x"] - self.__r__
+        x_max = self.__S__["x"] + self.__r__
+        y_min = self.__S__["y"] - self.__r__
+        y_max = self.__S__["y"] + self.__r__
+        x = np.linspace(x_min, x_max, self.__n__)
+        y = np.linspace(y_min, y_max, self.__n__)
         X, Y = np.meshgrid(x, y)
         C = X + Y*1j
         Z = np.zeros(C.shape, dtype=complex)
         mal_set = np.zeros(Z.shape)
-        for i in range(k):
-            Z = Z**2 + C
-            mal_set += 1 * (np.abs(Z)<2)
-        
-        return mal_set
-
-    def julia_set(self):
-        k = self.k
-        x, y = np.linspace(self.x_min, self.x_max, self.n), np.linspace(self.y_min, self.y_max, self.n)
+    
+        return mandelbrot_set(mal_set, Z, C, self.__k__)
+    
+    def __julia_set__(self):
+        x_min = self.__S__["x"] - self.__r__
+        x_max = self.__S__["x"] + self.__r__
+        y_min = self.__S__["y"] - self.__r__
+        y_max = self.__S__["y"] + self.__r__
+        x = np.linspace(x_min, x_max,self.__n__)
+        y = np.linspace(y_min, y_max, self.__n__)
         X, Y = np.meshgrid(x, y)
         Z = X + Y*1j
-        c = self.c_re + self.c_im*1j
+        c = self.__c_re__ + self.__c_im__*1j
         C = np.ones(Z.shape)*c
         jul_set = np.zeros(Z.shape)
-        for i in range(k):
-            Z = Z**2 + C
-            jul_set += 1 * (np.abs(Z) < 2)
-        
-        return jul_set
+        return julia_set(jul_set, Z, C, self.__k__)
     
     """Callback funkce pro radio buttony,
     které nastavují barevné mapování."""
     
-    def set_color(self, event):
+    def __set_color__(self, event):
         if event == "hot":
-            self.col = "hot"
-            self.im.set_cmap("hot")
+            self.__col__ = "hot"
+            self.__im__.set_cmap("hot")
             plt.draw()
         elif event == "magma":
-            self.col = "magma"
-            self.im.set_cmap("magma")
+            self.__col__ = "magma"
+            self.__im__.set_cmap("magma")
             plt.draw()
         elif event == "jet":
-            self.col = "jet"
-            self.im.set_cmap("jet")
+            self.__col__ = "jet"
+            self.__im__.set_cmap("jet")
             plt.draw()
         elif event == "rainbow":
-            self.col = "rainbow"
-            self.im.set_cmap("rainbow")
+            self.__col__ = "rainbow"
+            self.__im__.set_cmap("rainbow")
             plt.draw()
             
     """Metody nastavující hodnotu atributu k, c_re a c_im 
     (počet iterací, reálná a imaginnární část c) na hodnotu v textboxech."""
     
-    def set_k(self, k):
-        self.k = int(k)
+    def __set_k__(self, k):
+        self.__k__ = int(k)
         
-    def set_c_re(self, c_re):
-        self.c_re = float(c_re)
+    def __set_c_re__(self, c_re):
+        self.__c_re__ = float(c_re)
         
-    def set_c_im(self, c_im):
-        self.c_im = float(c_im)
+    def __set_c_im__(self, c_im):
+        self.__c_re__ = float(c_im)
         
-    def update(self, event):
-        M = self.fun_name()
-        self.im = self.ax.imshow(M, cmap=self.col, extent=(self.x_min, self.x_max, self.y_min, self.y_max))
+    def __update__(self, event):
+        x_min, x_max = self.__S__["x"] - self.__r__, self.__S__["x"] + self.__r__
+        y_min, y_max = self.__S__["y"] - self.__r__, self.__S__["y"] + self.__r__
+        M = self.__fun_name__()
+        self.__im__ = self.__ax__.imshow(M, cmap=self.__col__, extent=(x_min, x_max, y_min, y_max))
         plt.draw()
         
-    def update_re(self, event):
-        M = self.fun_name()
-        self.im = self.ax.imshow(M, cmap=self.col, extent=(self.x_min, self.x_max, self.y_min, self.y_max))
+    def __update_re__(self, event):
+        x_min, x_max = self.__S__["x"] - self.__r__, self.__S__["x"] + self.__r__
+        y_min, y_max = self.__S__["y"] - self.__r__, self.__S__["y"] + self.__r__
+        M = self.__fun_name__()
+        self.__im__ = self.__ax__.imshow(M, cmap=self.__col__, extent=(x_min, x_max, y_min, y_max))
         plt.draw()
         
-    def update_im(self, event):
-        M = self.fun_name()
-        self.im = self.ax.imshow(M, cmap=self.col, extent=(self.x_min, self.x_max, self.y_min, self.y_max))
+    def __update_im__(self, event):
+        M = self.__fun_name__()
+        self.__im__ = self.__ax__.imshow(M, cmap=self.__col__)
         plt.draw()
         
-    def zoom(self, event):
-        """Přibližuje nebo oddaluje fraktál na základě scrollování."""
-        x_center = (self.x_min + self.x_max) / 2
-        y_center = (self.y_min + self.y_max) / 2
-        x_dist = (self.x_max - self.x_min) / 2
-        y_dist = (self.y_max - self.y_min) / 2
+    def __zoom__(self, event):
         if event.button == 'up':
             # Přibližování fraktálu
-            self.x_min = x_center - x_dist / 2
-            self.x_max = x_center + x_dist / 2
-            self.y_min = y_center - y_dist / 2
-            self.y_max = y_center + y_dist / 2
+            self.__r__ *= 0.5
         elif event.button == 'down':
             # Oddalování fraktálu
-            self.x_min = x_center - 2 * x_dist
-            self.x_max = x_center + 2 * x_dist
-            self.y_min = y_center - 2 * y_dist
-            self.y_max = y_center + 2 * y_dist
-        M = self.fun_name()
-        self.im.set_data(M)
-        self.ax.set_xlim(self.x_min, self.x_max)
-        self.ax.set_ylim(self.y_min, self.y_max)
+            self.__r__ *= 2.0
+        M = self.__fun_name__()
+        x_min, x_max = self.__S__["x"] - self.__r__, self.__S__["x"] + self.__r__
+        y_min, y_max = self.__S__["y"] - self.__r__, self.__S__["y"] + self.__r__
+        self.__im__.set_data(M)
+        self.__ax__.set_xlim(x_min, x_max)
+        self.__ax__.set_ylim(y_min, y_max)
         plt.draw()
     
-    def on_key(self, event):
+    def __on_key__(self, event):
         """Zpracovává událost klávesnice a provádí
         funkci move_fractal ve směru dle stisknuté klávesy."""
         if event.key == 'up':
-            self.move_fractal('up')
+            self.__move_fractal__('up')
         elif event.key == 'down':
-            self.move_fractal('down')
+            self.__move_fractal__('down')
         elif event.key == 'left':
-            self.move_fractal('left')
+            self.__move_fractal__('left')
         elif event.key == 'right':
-            self.move_fractal('right')
+            self.__move_fractal__('right')
     
-    def move_fractal(self, direction):
+    def __move_fractal__(self, direction):
         """Pohybuje fraktálem v daném směru."""
         if direction == 'up':
-            self.y_min -= 0.1
-            self.y_max -= 0.1
+            self.__S__["y"] -= (0.1*self.__r__)
         elif direction == 'down':
-            self.y_min += 0.1
-            self.y_max += 0.1
+            self.__S__["y"] += (0.1*self.__r__)
         elif direction == 'left':
-            self.x_min -= 0.1
-            self.x_max -= 0.1
+            self.__S__["x"] -= (0.1*self.__r__)
         elif direction == 'right':
-            self.x_min += 0.1
-            self.x_max += 0.1
+            self.__S__["x"] += (0.1*self.__r__)
         # Generování nového fraktálu a aktualizace zobrazení
-        M = self.fun_name()
-        self.im.set_data(M)
-        self.ax.set_xlim(self.x_min, self.x_max)
-        self.ax.set_ylim(self.y_min, self.y_max)
+        M = self.__fun_name__()
+        x_min, x_max = self.__S__["x"] - self.__r__, self.__S__["x"] + self.__r__
+        y_min, y_max = self.__S__["y"] - self.__r__, self.__S__["y"] + self.__r__
+        self.__im__.set_data(M)
+        self.__ax__.set_xlim(x_min, x_max)
+        self.__ax__.set_ylim(y_min, y_max)
         plt.draw()
         
     def mand_fractal(self):
-        self.fun_name = self.mandelbrot_set
-        self.im = self.ax.imshow(self.fun_name(), cmap="hot", extent=(self.x_min, self.x_max, self.y_min, self.y_max))
+        x_min, x_max = self.__S__["x"] - self.__r__, self.__S__["x"] + self.__r__
+        y_min, y_max = self.__S__["y"] - self.__r__, self.__S__["y"] + self.__r__
+        self.__fun_name__ = self.__mandelbrot_set__
+        self.__im__ = self.__ax__.imshow(self.__fun_name__(), cmap="hot", extent=(x_min, x_max, y_min, y_max))
         plt.subplots_adjust(bottom=0.2)
         
         text_box_ax = plt.axes([0.2, 0.1, 0.2, 0.05])
-        text_box = w.TextBox(text_box_ax, 'iterations: ', initial=str(self.k))
-        text_box.on_submit(self.set_k)
+        text_box = w.TextBox(text_box_ax, 'iterations: ', initial=str(self.__k__))
+        text_box.on_submit(self.__set_k__)
         button_ax = plt.axes([0.4, 0.1, 0.1, 0.05])
         button = w.Button(button_ax, 'submit')
-        button.on_clicked(self.update)
+        button.on_clicked(self.__update__)
         
-        radio_ax = plt.axes([0.01, 0.8, 0.2, 0.15])
+        radio_ax = plt.axes([0.01, 0.8, 0.15, 0.15])
         radio_buttons = w.RadioButtons(radio_ax, ['hot', 'magma', 'jet', 'rainbow'])
-        radio_buttons.on_clicked(self.set_color)
+        radio_buttons.on_clicked(self.__set_color__)
         
-        self.fig.canvas.mpl_connect('key_press_event', self.on_key)
-        self.fig.canvas.mpl_connect('scroll_event', self.zoom)
+        self.__fig__.canvas.mpl_connect('key_press_event', self.__on_key__)
+        self.__fig__.canvas.mpl_connect('scroll_event', self.__zoom__)
         plt.show()
         
     def jul_fractal(self):
-        self.fun_name = self.julia_set
-        self.im = self.ax.imshow(self.julia_set(), cmap="hot", extent=(self.x_min, self.x_max, self.y_min, self.y_max))
+        x_min, x_max = self.__S__["x"] - self.__r__, self.__S__["x"] + self.__r__
+        y_min, y_max = self.__S__["y"] - self.__r__, self.__S__["y"] + self.__r__
+        self.__fun_name__ = self.__julia_set__
+        self.im = self.__ax__.imshow(self.__julia_set__(), cmap="hot", extent=(x_min, x_max, y_min, y_max))
         plt.subplots_adjust(bottom=0.2)
         
         text_box_it = plt.axes([0.2, 0.1, 0.07, 0.05])
-        text_box = w.TextBox(text_box_it, 'iterations: ', initial=str(self.k))
-        text_box.on_submit(self.set_k)
+        text_box = w.TextBox(text_box_it, 'iterations: ', initial=str(self.__k__))
+        text_box.on_submit(self.__set_k__)
         
         button_it = plt.axes([0.27, 0.1, 0.1, 0.05])
         sub_it_button = w.Button(button_it, 'submit')
-        sub_it_button.on_clicked(self.update)
+        sub_it_button.on_clicked(self.__update__)
         
         text_box_c_re = plt.axes([0.45, 0.1, 0.07, 0.05])
-        text_box_re = w.TextBox(text_box_c_re, 'c_re: ', initial=str(self.c_re))
-        text_box_re.on_submit(self.set_c_re)
+        text_box_re = w.TextBox(text_box_c_re, 'c_re: ', initial=str(self.__c_re__))
+        text_box_re.on_submit(self.__set_c_re__)
         
         button_c_re = plt.axes([0.52, 0.1, 0.1, 0.05])
         button_re = w.Button(button_c_re, 'submit')
-        button_re.on_clicked(self.update_re)
+        button_re.on_clicked(self.__update_re__)
         
         
         text_box_c_im = plt.axes([0.7, 0.1, 0.07, 0.05])
-        text_box_im = w.TextBox(text_box_c_im , 'c_im', initial=str(self.c_im))
-        text_box_im.on_submit(self.set_c_im)
+        text_box_im = w.TextBox(text_box_c_im , 'c_im', initial=str(self.__c_im__))
+        text_box_im.on_submit(self.__set_c_re__)
         
         button_c_im = plt.axes([0.77, 0.1, 0.1, 0.05])
         button_im = w.Button(button_c_im, 'submit')
-        button_im.on_clicked(self.update_im)
+        button_im.on_clicked(self.__update_im__)
         
         radio_ax = plt.axes([0.0, 0.7, 0.2, 0.15])
         radio_buttons = w.RadioButtons(radio_ax, ['hot', 'magma', 'jet', 'rainbow'])
-        radio_buttons.on_clicked(self.set_color)
-        self.fig.canvas.mpl_connect('key_press_event', self.on_key)
-        self.fig.canvas.mpl_connect('scroll_event', self.zoom)
+        radio_buttons.on_clicked(self.__set_color__)
+        self.__fig__.canvas.mpl_connect('key_press_event', self.__on_key__)
+        self.__fig__.canvas.mpl_connect('scroll_event', self.__zoom__)
         plt.show()
 
 if __name__ == "__main__":
     test = Fractal(1000)
-    test.mand_fractal()
+    test.jul_fractal()
